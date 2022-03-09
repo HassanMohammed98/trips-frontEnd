69e6884a23d9e6f2cdecb9027593213c307b9da9
Hassan Mohammed <hassan-n98@hotmail.com>
2022-03-09 21:52:00 +0300

    add header component

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
+import { HStack } from "native-base";
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.header}>
+      <HStack w={"50%"} style={styles.reg}>
+        <Button onPress={() => navigation.navigate("SignIn")} title="SignIn" />
+        <Button onPress={() => navigation.navigate("SignUp")} title="SignUp" />
+      </HStack>
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    width: "100%",
+    height: "10%",
+    backgroundColor: "grey",
+    justifyContent: "center",
+    alignItems: "center",
+    // borderBottomStartRadius: 5000,
+    // borderBottomEndRadius: 5000,
+  },
+});
--- components/Home.js
+++ components/Home.js
@@ -1,24 +1,12 @@
 import { StyleSheet, Text, View, ScrollView, Button } from "react-native";
 import React from "react";
 import TripList from "./Trip/TripList";
-import { HStack } from "native-base";
+import Header from "./Header";
 
 const Home = ({ navigation }) => {
   return (
     <View style={styles.homePage}>
-      {/* <Text>Home</Text> */}
-      <View style={styles.header}>
-        <HStack w={"50%"} style={styles.reg}>
-          <Button
-            onPress={() => navigation.navigate("SignIn")}
-            title="SignIn"
-          />
-          <Button
-            onPress={() => navigation.navigate("SignUp")}
-            title="SignUp"
-          />
-        </HStack>
-      </View>
+      <Header />
       <ScrollView style={styles.TripsList}>
         <TripList />
 
@@ -34,15 +22,6 @@ const Home = ({ navigation }) => {
 export default Home;
 
 const styles = StyleSheet.create({
-  header: {
-    width: "100%",
-    height: "10%",
-    backgroundColor: "grey",
-    justifyContent: "center",
-    alignItems: "center",
-    // borderBottomStartRadius: 5000,
-    // borderBottomEndRadius: 5000,
-  },
   homePage: {
     width: "100%",
     height: "100%",
