e2e183c92a0877ee76dc70462b6555d70020b210
Abdullah90 <darkside944@gmail.com>
2022-03-11 11:02:46 +0300

    trip Detail & it's desgin

--- stores/instance.js
+++ stores/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://192.168.1.54:5000";
+export const baseURL = "http://192.168.8.147:5000";
 
 export const instance = axios.create({
   baseURL: `${baseURL}/api`,
